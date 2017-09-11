43db1fede8f89814e67b4f960b6a06cae8c171cf
MANI <pavank.m@outlook.in>
2017-09-11 15:45:15 +0530

    Add method modified according to requirement

--- src/main/java/com/amazon/HelloWorld.java
+++ src/main/java/com/amazon/HelloWorld.java
@@ -16,14 +16,19 @@ public class HelloWorld
 	{
 		System.out.println("I am from add method");
 		c=a+b;
+		System.out.println("Sum of two values is:"+c);
 		return c;
 	}
     public static void main(String args[])
 	{
+    	HelloWorld obj = new HelloWorld();
+    	System.out.println(obj.add());
+    	//System.out.println(obj.c);
 		System.out.println("Inside Main block");
 		for(int i=1;i<=5;i++)
 		{
 			System.out.println("Hello Jenkins:---------" +i);
+			
 		}
 		System.out.println("Outside Main block");
 	    System.out.println("Exited from Program");
